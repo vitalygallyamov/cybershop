70605bd3335996a7e6d7bb2a85981dd4831e9259
Gallyamov Vitaly <vitgvr@gmail.com>
2013-08-04 14:57:37 +0600

    add teplate settings file

---
+++ template.settings.inc.php
@@ -0,0 +1,18 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'prestashop');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', 'root');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'MyISAM');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_MEDIA_SERVER_1_', '');
+define('_MEDIA_SERVER_2_', '');
+define('_MEDIA_SERVER_3_', '');
+define('_COOKIE_KEY_', 'HcuIJFyT0LYnG9Z6vSXZhO1mZ9o8wo6XVxku6eJETsWgm9WxMXEcx4Ov');
+define('_COOKIE_IV_', 'xKsa0YA2');
+define('_PS_CREATION_DATE_', '2013-08-04');
+define('_PS_VERSION_', '1.5.4.1');
+define('_RIJNDAEL_KEY_', 'Xv2RUgFtYxDZeHKcw8WNTnjYKGdDUG3k');
+define('_RIJNDAEL_IV_', 'FqxRWU4oap2FJvPdYPffDA==');
